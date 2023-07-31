b4941d27572aa7727a613195853742192e14c9d0
GBR <gb.gl@hotmail.com>
2023-07-31 18:08:24 -0300

    Created userSchema

---
+++ src/models/user.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+    {
+        id: { type: String },
+        name: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        phone: { type: Number, required: true },
+        adress: { type: String, required: true },
+        zipCode: { type: Number, required: true },
+        city: { type: String, required: true },
+        state: { type: String, required: true },
+        country: { type: String, required: true },
+
+    }
+);
+
+const users = mongoose.model('users', userSchema);
+
+export default users;
