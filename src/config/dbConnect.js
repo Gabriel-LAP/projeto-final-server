0bbc487398a4357ab809c7030e036ff85a2b8ff8
GBR <gb.gl@hotmail.com>
2023-07-31 18:32:33 -0300

    Added db user and password on a .env file

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://gabriel:123@cluster0.ii1tyrp.mongodb.net/?retryWrites=true&w=majority");
+const dbUser = PROCESS.env.DB_USER
+const dbPassword = PROCESS.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.ii1tyrp.mongodb.net/?retryWrites=true&w=majority`);
 
 let db = mongoose.connection;
 
